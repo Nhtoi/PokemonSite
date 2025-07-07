8189b2e2e3863454ea36a5528c46fad11103ae36
Nhtoi <krruizsimon@gmail.com>
2025-07-07 19:53:10 -0400

    Refractored to use class and object for pokemon

--- DamageRelations.js
+++ DamageRelations.js
@@ -1,17 +1,38 @@
-const final = {
-    "teamWeakness": new Set(),
-    "teamEffective": new Set()
-}
+
 
 export async function PopulateArray(array){
-    await getURL(array)
-    return final
+    return await getURL(array)
+}
+
+export async function singleDamageRelation(url){
+    return getSingle(url)
+}
+async function getSingle(array) {
+    const relations = {
+    "weakness": new Set(),
+    "effective": new Set()
+    }
+    //console.log("Array in test.js", array)
+    for (const item of array) {
+            //console.log("item in test.js", item)
+            const data = await fetch(`${item}`)
+            const jsonData = await data.json()
+            const damageRelations = await GetDamageRelations(jsonData)
+    
+            for (const type of Object.values(damageRelations.weaknesses)) {
+                relations.weakness.add(type)
+            }
+            for (const type of Object.values(damageRelations.effectiveness)) {
+                relations.effective.add(type)
+            }
+        }
+    return relations
 }
 
 async function getURL(array) {
-    console.log("Array in test.js", array)
+    //console.log("Array in test.js", array)
     for (const item of array) {
-            console.log("item in test.js", item)
+            //console.log("item in test.js", item)
             const data = await fetch(`${item}`)
             const jsonData = await data.json()
             const relations = await GetDamageRelations(jsonData)
@@ -22,7 +43,7 @@ async function getURL(array) {
 
 async function GetDamageRelations(data){
     let res = data
-    console.log("from GetDamageRelations ", res)
+    //console.log("from GetDamageRelations ", res)
     const weak = res.damage_relations.double_damage_from
     const effective = res.damage_relations.double_damage_to
     const relations = {}
@@ -30,11 +51,11 @@ async function GetDamageRelations(data){
     const effectiveness = {}
 
     for (let i = 0; i < weak.length; i++) {
-        //console.log(`Weak Against, ${weak[i].name}`)
+        // console.log(`Weak Against, ${weak[i].name}`)
         weaknesses[i] = weak[i].name
     }
       for (let i = 0; i < effective.length; i++) {
-        //console.log(`Strong Against, ${effective[i].name}`)
+        // console.log(`Strong Against, ${effective[i].name}`)
         effectiveness[i] = effective[i].name
     
     }
@@ -43,10 +64,19 @@ async function GetDamageRelations(data){
     return relations
 }
 
-function DisplayDamageRelations(damageRelations){
-    final.teamWeakness.add(damageRelations.weaknesses)
-    final.teamEffective.add(damageRelations.effectiveness)
-    return final
-} 
 
-//TODO: GET SINGULAR POKEMON DAMAGE RELATIONS
+
+function DisplayDamageRelations(damageRelations) {
+    const final = {
+    "teamWeakness": new Set(),
+    "teamEffective": new Set()
+    }
+    for (const type of Object.values(damageRelations.weaknesses)) {
+        final.teamWeakness.add(type);
+    }
+    for (const type of Object.values(damageRelations.effectiveness)) {
+        final.teamEffective.add(type);
+    }
+    return final;
+}
+
--- app.js
+++ app.js
@@ -1,24 +1,45 @@
-//!BIG TODO: REFRACTOR TO USE OBJECTS INSTEAD OF PASSING PROPERTIES BY HTML VALUES
-
-import {PopulateArray} from '/DamageRelations.js'
-
+import {PopulateArray, singleDamageRelation} from '/DamageRelations.js'
 const subButton = document.getElementById("subButton")
 const clearButton = document.getElementById("clear")
 const shiny = document.getElementById("shinycheck")
-const body = document.querySelector("body")
 const imageContainer = document.getElementById("image-container")
-let typesSet = new Set()
+const pokemons = []
+const pokemonContainers = []
+const typesSet = new Set()
+const typeColorsPromise = fetch('colors.json').then(res => res.json());
 
-let typeColorsPromise = fetch('colors.json').then(res => res.json());
 //team, team.pokemon, team.pokemon.types team.pokemon.name so team is an object that has pokemon as an object
-let team = {
+const team = {
     "team": [],
     "allTypes": new Set()
 }
+
 let inputPokemon;
 let isShiny = false
-let pokemonContainers = []
-let sound;
+
+
+class Pokemon{
+    constructor(name, sprite, cry, typings, typesURL, damageRelations){
+        this.name = name
+        this.sprite = sprite
+        this.cry = cry
+        this.typings = typings
+        this.typesURL = typesURL 
+        this.damageRelations = null
+    }
+    addToTeam(){
+        team.team.push(this)
+        console.log(team.team)
+    }
+    playSound(){
+        const audio = new Audio(this.cry);
+        audio.volume = 0.06;
+        audio.play()
+    }
+    async getDamageRelations(){
+         this.damageRelations = await singleDamageRelation(this.typesURL)
+    }
+}
 
 shiny.addEventListener("change", ()=>{
     if(shiny.checked){
@@ -27,6 +48,7 @@ shiny.addEventListener("change", ()=>{
         isShiny = false
     }
 })
+
 function clear(target){
     if (target === undefined || target === null){
       let clearTarget = document.getElementById("image-container")
@@ -44,22 +66,21 @@ clearButton.addEventListener("click", ()=>{
 })
 subButton.addEventListener("click", () =>{
     const input = document.getElementById("searchPokemon")
-    inputPokemon = input.value
+    inputPokemon = input.value.toLowerCase()
     CreatePokemon(setUrl(inputPokemon))
 })
+
 function setUrl(inputPokemon){
-    const pokemon = document.getElementById("searchPokemon")
     let url = `https://pokeapi.co/api/v2/pokemon/${inputPokemon}`
     return url
 }
-async function getData(url){
 
+async function getData(url){
     try {
         let res = await fetch(url)
         if(!res.ok){
             console.error("Pokemon Not Found")
         }
-
         res = await res.json() 
         console.log(res)
         return res
@@ -68,31 +89,30 @@ async function getData(url){
         console.error("Pokemon Not Found")
     }
 }
-let typing = []
 
 async function CreatePokemon(url){
+    const data = await getData(url)
+    
     const pokemonContainer = document.createElement("div")
     pokemonContainer.setAttribute("id", "pokemonContainer") 
     let sprite = ""
-    const data = await getData(url)
-    // console.log(sound)
+
     if (isShiny){
         sprite = data.sprites.front_shiny
     } else {
         sprite = data.sprites.front_default
     }
+    
     const img = document.createElement("img")
     const typingDiv = document.createElement("div")
+
     typingDiv.setAttribute("id", "pokemon")
-    typingDiv.setAttribute("name", `${inputPokemon}`)
-    typingDiv.setAttribute("sound", `${data.cries.latest}`)
+    
     const types = [...data.types]
-    //TypesInfo(data.types)
     const colors = await typeColorsPromise
-    //console.log(colors)
+    
     let count = 1
     for (const typing of types) {
-        //console.log(typing.type.name)
         let typeTextDiv = document.createElement("div")
         typeTextDiv.setAttribute("id",`type${count}`)
         typeTextDiv.setAttribute("class", colors[typing.type.name] )
@@ -100,33 +120,42 @@ async function CreatePokemon(url){
         count += 1
         typingDiv.append(typeTextDiv)
     }
-    //console.log(typingDiv)
     count = 0
-    img.setAttribute("src", `${sprite}`)
+    
+    
+    const typesURL = TypesInfo(data)
+    let damageRelations;
+    const newPokemon = new Pokemon(inputPokemon, sprite, data.cries.latest, types, typesURL, damageRelations)
+    await newPokemon.getDamageRelations()
+    console.log('HERRE', newPokemon.damageRelations)
+    pokemons.push(newPokemon)
+    
+    
+    typingDiv.setAttribute("name", `${newPokemon.name}`)
+    img.setAttribute("src", `${newPokemon.sprite}`)
+
     pokemonContainer.append(typingDiv)
     pokemonContainer.append(img)
+    
     imageContainer.append(pokemonContainer)
     pokemonContainers.push(pokemonContainer)
+
+    console.log(pokemonContainers)
     const addPokemon = document.createElement("button")
     addPokemon.setAttribute("id", "AddButton")
     addPokemon.setAttribute("class", "button")
     addPokemon.innerText = "Add Pokemon"
+    
     for (const container of pokemonContainers){
             container.append(addPokemon)
     }
     addPokemon.addEventListener("click", (e)=>{
-        TypesInfo(data.types)
         let workingOn = e.target.parentElement
-        sound = workingOn.children[0].getAttribute("sound")
-        const audio = new Audio(sound);
-        audio.volume = 0.06;
-        audio.play()
-        console.log("Image", workingOn.children[1])
-        team.team.push(workingOn.children[1])
-        console.log(team.team)
-        //team.allTypes.add(TypesInfo(data.types))
-        // console.log(team.allTypes)
-        teamMaker(workingOn.children[1])  
+        const pokeName = workingOn.children[0].getAttribute("name")
+        const selectedPokemon = pokemons.find(p => p.name === pokeName)
+        selectedPokemon.addToTeam()
+        selectedPokemon.playSound()
+        teamMaker(selectedPokemon.sprite)  
         clear(e.target.parentElement)
         
     })
@@ -135,34 +164,32 @@ async function CreatePokemon(url){
 let counter = 0;
 const teamContainer = document.getElementById("team-container")
 const completedTeam = document.getElementById("completed-team")
+
 async function teamMaker(pokemonToAdd) {
-    const image = pokemonToAdd.getAttribute("src")
     const teamDiv = document.createElement("img")
-    teamDiv.setAttribute("src",`${image}`)
+    teamDiv.setAttribute("src",`${pokemonToAdd}`)
     teamContainer.append(teamDiv)
-    //! IMPORTANT HAVE THIS SET TO 3 INSTEAD OF 6 FOR TESTING
-    if (team.team.length === 3){
+    if (team.team.length === 6){
         let finishedTeam = document.createElement("div")
         finishedTeam.setAttribute("id", `team-number${counter}`)
         let addAll = Array.from(teamContainer.children)
         addAll.forEach(child => {
         finishedTeam.append(child)
-        //console.log(team.team)
         })       
         const teamDamageRelations = await PopulateArray(Array.from(typesSet)) 
-        console.log("Readin from here", teamDamageRelations)
         completedTeam.append(finishedTeam)    
         counter += 1
         team.team = []
-        typesSet = new Set()
     }
 }
 
 function TypesInfo(data){
-    for (const type of data) {
-        typesSet.add(type.type.url)
+    const types = new Set()
+    console.log(typeof(data.types))
+    for (const type of data.types) {
+        types.add(type.type.url)
     }
-    console.log("HERE",typesSet)
+    return types
 }
 
 //TODO: DISPLAY TEAM DAMAGE RELATIONS
